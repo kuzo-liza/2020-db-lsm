c033c115255f4e616a60015b794be21931ae9662
kuzo-liza <kuzo-liza@yandex.ru>
2020-04-20 18:26:33 +0300

    fixes

--- src/main/java/ru/mail/polis/kuzoliza/MyDAO.java
+++ src/main/java/ru/mail/polis/kuzoliza/MyDAO.java
@@ -1,4 +1,4 @@
-package ru.mail.polis.kuzo_liza;
+package ru.mail.polis.kuzoliza;
 
 import com.google.common.collect.Iterators;
 import org.jetbrains.annotations.NotNull;
@@ -17,19 +17,22 @@ public class MyDAO implements DAO {
 
     @NotNull
     @Override
-    public Iterator<Record> iterator(@NotNull ByteBuffer from) throws IOException {
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) throws IOException {
         return Iterators.transform(
                 map.tailMap(from).entrySet().iterator(),
-                element -> Record.of(element.getKey(), element.getValue()));
+                element -> {
+                    assert element != null;
+                    return Record.of(element.getKey(), element.getValue());
+                });
     }
 
     @Override
-    public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) throws IOException {
+    public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) throws IOException {
         map.put(key, value);
     }
 
     @Override
-    public void remove(@NotNull ByteBuffer key) throws IOException {
+    public void remove(@NotNull final ByteBuffer key) throws IOException {
         map.remove(key);
     }
 
